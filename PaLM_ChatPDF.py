5afd2f8aa552c45ccfd4bad51c4dbc96fc8a7fe7
Vedant Dwivedi <Vedant.dwivedi55@gmail.com>
2023-10-10 12:41:24 +0530

    Update PaLM_ChatPDF.py

--- PaLM_ChatPDF.py
+++ PaLM_ChatPDF.py
@@ -44,8 +44,8 @@ def main():
     
     #uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
     #if uploaded_file is not None:
-    files_path = "https://publuu.com/flip-book/263252/619188"
-    #files_path = "./Products offer by yash computech solutions.pdf"
+    #files_path = "https://publuu.com/flip-book/263252/619188"
+    files_path = "./Products offer by yash computech solutions.pdf"
     loaders = [UnstructuredPDFLoader(files_path)]
 
 
